129ed5f95277b3321789c3662348dbf01586fb03
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-11-14 16:57:11 -0300

    querys do projeto

--- database/database.sql
+++ database/database.sql
@@ -24,3 +24,25 @@ create table usuarios_seguidores(
     id_usuario int not null,
     id_usuario_seguindo int not null
 );
+insert into usuarios(id, nome, email, senha)values(30, 'Teste', 'teste@teste.com', '81dc9bdb52d04dc20036dbd8313ed055');
+
+insert into usuarios (id, nome)values
+(1, 'Guilherme Amaral'),
+(2,'Elon Musk'),
+(3,'Cristiano Ronaldo'), 
+(4,'Gran Toro'), 
+(5,'Scalene'),
+(10,'Fernanda'),
+(15,'Ton'),
+(20,'Mark Zuckerberg');
+
+insert into tweets (id_usuario, tweet)values
+(4, 'Siga @grantorobanda no instagram!'),
+(2,'Founder, CEO and Technical Director of SpaceX; CEO of Tesla, Inc.; vice president of OpenAI, 
+founder and CEO of Neuralink; co-founder, president of SolarCity and owner of Twitter.'),
+(2, 'I will not sue guilherme amaral!'),
+(15,'Front man da Gran Toro'),
+(1, 'Entre em contato comigo através do meu email! gui.amaral123@outlook.com'),
+(1,'Eu criei uma conta no github  github.com/Guimrl/');
+
+insert into usuarios_seguidores(id_usuario, id_usuario_seguindo)value(30,1),(30,2),(30,3),(30,4),(30,5),(30,10),(30,15),(30,20);
